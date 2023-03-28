bfacaec1b7cce7a8d8291a7c8cc1a1bec3ab34e0
vishnu <vishnu981203@gmail.com>
2023-03-28 06:57:54 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Details } from "../src/components/data/data.components";
 
-function App() {
+const App = () => {
+  const Data = [...Details];
+  console.log(Data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>
+        <li>{Data.id}</li>
+        <li>{Data.name}</li>
+        <li>{Data.designation}</li>
+      </ul>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/data/data.components.js
@@ -0,0 +1,17 @@
+export const Details = [
+  {
+    id: 7,
+    name: "Dhoni",
+    designation: "Captain",
+    img: "",
+  },
+  { id: 18, name: "Virat", designation: "Right-Hand-Batsman" },
+  { id: 3, name: "Raina", designation: "Left-Hand-Batsman" },
+  { id: 45, name: "Rohit", designation: "Right-Hand-Batsman" },
+  { id: 93, name: "Bumrah", designation: "Bowler" },
+  { id: 33, name: "Hardik", designation: "All-Rounder" },
+  { id: 1, name: "KL Rahul", designation: "Right-Hand-Batsman" },
+  { id: 8, name: "Jadeja", designation: "All-Rounder" },
+  { id: 15, name: "Bhuvaneshwar Kumar", designation: "Bowler" },
+  { id: 13, name: "Siraj", designation: "Bowler" },
+];
